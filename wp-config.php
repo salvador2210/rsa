c98ed90b0a70a1be6ebef8a4256e9acc995cd79c
Eduardo Ortega <ortegon000@hotmail.com>
2017-03-13 16:40:11 -0600

    x

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'rsa');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '123');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
